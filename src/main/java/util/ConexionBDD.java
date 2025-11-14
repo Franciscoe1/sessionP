1f4f9dfec702cb34ae5503728933cbaa4cf023bf
Franciscoe1 <franciscoxav522@gmail.com>
2025-11-14 12:03:30 -0500

    t

---
+++ src/main/java/util/ConexionBDD.java
@@ -0,0 +1,14 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBDD {
+    private static String url= "jdbc:mysql://localhost:3306/sistemaventas?servreTimezone=UTC";
+    private static String username="root";
+    private static String password="";
+    public static Connection getConnection()throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
+}
